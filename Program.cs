9caa8032f7d3e6ecacb943b1228f54b911fdc82a
DEV <dev@info.local>
2020-12-18 13:48:54 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using _18_12_pesencial.classes;
+
+namespace _18_12_pesencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int codigo;
+            string nome;
+            string descricao;
+            int estoque;
+
+            Console.Clear();
+            Console.WriteLine("Cadastro de mercadoria");
+
+            Console.Write("Codigo do produto: ");
+            codigo = int.Parse(Console.ReadLine());
+            
+            Console.Write("Nome do produto: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Descrição do produto: ");
+            descricao = Console.ReadLine();
+
+            Console.Write("Quantidade no estoque: ");
+            estoque = int.Parse(Console.ReadLine());
+
+            Produto produto1 = new Produto(codigo, nome, descricao, estoque);
+
+            Console.WriteLine("\nInformações do produto:");
+            Console.WriteLine("==================================");
+            Console.WriteLine($"CÓDIGO: {produto1.Codigo}");
+            Console.WriteLine($"NOME: {produto1.Nome}");
+            Console.WriteLine($"DESCRIÇÃO: {produto1.Descricao}");
+            Console.WriteLine($"ESTOQUE: {produto1.Estoque}");
+            Console.WriteLine("==================================");
+        }
+    }
+}
